132098f6642ec447cd48df0969ea24b148305247
EvGamer <1evgenbetake@gmail.com>
2021-03-17 23:52:28 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import "regenerator-runtime/runtime.js";
-import Engine from './Engine'
+import Editor from "./Editor";
+import Engine from "./Engine";
 
 const WIDTH = 1024;
 const HEIGHT = 768;
 
-window.onload = function () {
+function main() {
   const engine = new Engine(document, WIDTH, HEIGHT);
-  engine.attachToElement(document.getElementById('canvas-container'));
-  engine.render();
-  setInterval(() => {engine.update()}, 50);
-  setInterval(() => {engine.renderAgents()}, 5);
-};
+  const canvasContainer = document.getElementById("canvas-container");
+  engine.attachToElement(canvasContainer);
+  engine.renderAll();
+  setInterval(() => {
+    engine.update();
+  }, 50);
+  setInterval(() => {
+    engine.renderAgents();
+  }, 5);
+
+  const editor = new Editor({
+    onAddNode: (x, y) => engine.handleEditorAddNode(x, y),
+    onSelectNode: (x, y) => engine.handleEditorSelectNode(x, y),
+    onAddEdge: (nodeId, x, y) => engine.handleEditorAddEdge(nodeId, x, y)
+  });
+
+  document.getElementById("add-node").addEventListener("click", (event) => {
+    editor.handleAddNodeButtonClick(event);
+  });
+
+  document.getElementById("add-edge").addEventListener("click", (event) => {
+    editor.handleAddEdgeButtonClick(event);
+  });
+
+  canvasContainer.addEventListener("mouseup", (event) => {
+    editor.handleMouseClick(event);
+  });
+}
+
+main();
